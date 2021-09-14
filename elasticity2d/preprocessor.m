a0320158d5fe8aca05328702e4a727914e1f4d31
zhangbiao12121 <309957850@qq.com>
2021-09-14 17:00:39 +0800

    将elasticy2d提交在mpm仓库

---
+++ elasticity2d/preprocessor.m
@@ -0,0 +1,10 @@
+function [K,f,d] = preprocessor; 
+include_flags;
+
+% read input file 
+%input_file_1ele; 
+input_file_16ele; 
+%input_file_64ele;
+
+% generate ID and LM arrays
+d = setup_ID_LM(d);
